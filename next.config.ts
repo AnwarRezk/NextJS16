0160ae9a48f249aafe7f0654fbdcc92269d59aa0
Anwar Rezk <arezk@silicon-mind.com>
2025-11-29 00:57:12 +0200

    Implement Homepage with project setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    //Enable turbo file system caching
+    turbopackFileSystemCacheForDev: true
+  },
 };
 
 export default nextConfig;
